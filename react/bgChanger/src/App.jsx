d70557adfc8bf86e530a73c5c4b58743d2a95f3b
sachinchandrabhatt <ms20051502@gmail.com>
2024-12-16 16:08:43 +0530

    checking shadcn

---
+++ react/bgChanger/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import {Button} from "@shadcn/ui"
+// import Button from '@shadcn_ui'; // Default import
+
+
+function App() {
+
+  return (
+    <>
+      <Button>Click Me</Button>
+    </>
+  )
+}
+
+export default App
---
+++ react/bgChanger/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}',  // adjust the paths to your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
